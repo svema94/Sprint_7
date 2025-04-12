96256319778d1423df1e49c9dab161d6e5cb80de
Pylaeva Tatyana <radzivlyuk-t@yandex.ru>
2025-04-12 18:50:42 +0300

    Corrections after the review2

--- src/test/java/CourierLoginTest.java
+++ src/test/java/CourierLoginTest.java
@@ -1,6 +1,7 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.After;
+import org.junit.Before;
 import org.example.CourierSteps;
 import org.example.NewCourier;
 import org.example.CourierLogin;
@@ -16,69 +17,58 @@ public class CourierLoginTest {
     public static String wrongPassword = "9876";
     public static String wrongLogin = "ninjaj123a";
 
+    private CourierSteps courierSteps;
+    private NewCourier courierCreateRequest;
+    private CourierLogin courierLoginRequest;
+
+    @Before
+    public void createCourier() {
+        courierSteps = new CourierSteps();
+        courierCreateRequest = new NewCourier(login, password, firstName);
+        courierSteps.courierCreate(courierCreateRequest);
+    }
+
     @Test
     @DisplayName("Авторизация курьера")
     @Description("Проверка авторизации курьера с набором валидных данных")
     public void loginCourier() {
-
-        NewCourier courierCreateRequest = new NewCourier(login, password, firstName);
-        CourierLogin courierLoginRequest = new CourierLogin(login, password);
-        CourierSteps courierSteps = new CourierSteps();
-
-        courierSteps.courierCreate(courierCreateRequest);
+        courierLoginRequest = new CourierLogin(login, password);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(200);
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().body("id", instanceOf(Integer.class));
-
     }
 
     @Test
     @DisplayName("Авторизация курьера без логина")
     @Description("Проверка невозможности авторизации без передачи поля login")
     public void loginCourierWithoutLogin() {
-
-        NewCourier courierCreateRequest = new NewCourier(login, password, firstName);
-        CourierLogin courierLoginRequest = new CourierLogin(null, password);
-        CourierSteps courierSteps = new CourierSteps();
-
-        courierSteps.courierCreate(courierCreateRequest);
+        courierLoginRequest = new CourierLogin(null, password);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(400);
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().body("message", equalTo("Недостаточно данных для входа"));
-
-
     }
 
     @Test
     @DisplayName("Авторизация курьера без пароля")
     @Description("Проверка невозможности авторизации без передачи поля password")
     public void loginCourierWithoutPassword() {
-
-        NewCourier courierCreateRequest = new NewCourier(login, password, firstName);
-        CourierLogin courierLoginRequest = new CourierLogin(login, null);
-        CourierSteps courierSteps = new CourierSteps();
-
-        courierSteps.courierCreate(courierCreateRequest);
+        courierLoginRequest = new CourierLogin(login, null);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(400);
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().body("message", equalTo("Недостаточно данных для входа"));
-
-
     }
 
     @Test
     @DisplayName("Авторизация курьера c несуществующей парой логин-пароль")
     @Description("Проверка невозможности авторизации с несуществующими данными для входа")
-    public void courierLoginWithNonExistentCredential () {
-
-        CourierLogin courierLoginRequest = new CourierLogin(login, password);
-        CourierSteps courierSteps = new CourierSteps();
+    public void courierLoginWithNonExistentCredential() {
+        courierLoginRequest = new CourierLogin(login, password);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(404);
@@ -89,10 +79,8 @@ public class CourierLoginTest {
     @Test
     @DisplayName("Авторизация курьера c неверным паролем")
     @Description("Проверка невозможности авторизации c неверным паролем")
-    public void courierLoginWithWrongPassword () {
-        NewCourier courierCreateRequest = new NewCourier(login, password, firstName);
-        CourierLogin courierLoginRequest = new CourierLogin(login, wrongPassword);
-        CourierSteps courierSteps = new CourierSteps();
+    public void courierLoginWithWrongPassword() {
+        courierLoginRequest = new CourierLogin(login, wrongPassword);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(404);
@@ -103,10 +91,8 @@ public class CourierLoginTest {
     @Test
     @DisplayName("Авторизация курьера c неверным логином")
     @Description("Проверка невозможности авторизации c неверным логином")
-    public void courierLoginWithWrongLogin () {
-        NewCourier courierCreateRequest = new NewCourier(login, password, firstName);
-        CourierLogin courierLoginRequest = new CourierLogin(wrongLogin, password);
-        CourierSteps courierSteps = new CourierSteps();
+    public void courierLoginWithWrongLogin() {
+        courierLoginRequest = new CourierLogin(wrongLogin, password);
 
         courierSteps.courierLogin(courierLoginRequest)
                 .assertThat().statusCode(404);
@@ -120,5 +106,4 @@ public class CourierLoginTest {
         CourierLogin validCourierLoginRequest = new CourierLogin(login, password);
         courierSteps.courierDeleteAfterLogin(validCourierLoginRequest);
     }
-
 }
